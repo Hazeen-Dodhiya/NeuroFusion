f2d6cccf8bcc917ad6a8c5bc04228a1618bcc02c
hazeen dodhiya <hazeenwork@gmail.com>
2026-05-12 18:17:30 +0500

    added config file drive.js logic

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,5 @@
 const User = require("../models/User");
+const MRI = require("../models/MRI")
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const crypto = require('crypto')
